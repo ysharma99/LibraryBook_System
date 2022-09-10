af56df906dadfbcc34c2f04071fd0c4cf1b4ff57
EnricoLLC <enricoc.14116@gmail.com>
2022-09-10 20:51:54 +1200

    Created db setup file
    References #2

---
+++ setup-database.sql
@@ -0,0 +1,49 @@
+CREATE TABLE book (
+  bookID varchar(10),
+  title varchar(50) NOT NULL,
+  author varchar(50) NOT NULL,
+  PRIMARY KEY (bookID)
+);
+
+INSERT INTO book VALUES ('000', 'To Kill A Mockingbird', 'Harper Lee');
+INSERT INTO book VALUES ('001', 'The Name of the Wind', 'Patrick Rothfuss');
+INSERT INTO book VALUES ('002', 'The Hunger Games', 'Suzanne Collins');
+
+CREATE TABLE administrator (
+  adminID varchar(10),
+  username varchar(50) NOT NULL,
+  userPWD varchar(50) NOT NULL,
+  PRIMARY KEY (adminID)
+);
+
+INSERT INTO administrator VALUES ('000', 'admin123', 'Password123');
+INSERT INTO administrator VALUES ('001', 'admin321', 'Adm1nOne!');
+
+CREATE TABLE customer (
+  customerID varchar(10),
+  username varchar(50) NOT NULL,
+  userPWD varchar(50) NOT NULL,
+  fName varchar(50) NOT NULL,
+  lname varchar(50) NOT NULL,
+  stAdress varchar(50) NOT NULL,
+  phNumber varchar(50) NOT NULL,
+  PRIMARY KEY (customerID)
+);
+
+INSERT INTO customer VALUES ('000', 'Yashmellow99','Password123', 'Yashree', 'Sharma', 'somewhere', '224');
+INSERT INTO customer VALUES ('001', 'Nicy_Spicy','Adm1nOne!', 'Enrico', 'Condino', 'over the rainbow', '123');
+
+CREATE TABLE customerBooks (
+  customerID varchar(10) references customer(customerID),
+  bookID varchar(10) references book(bookID),
+  constraint pk_customerBooks PRIMARY KEY (customerID, bookID)
+);
+
+INSERT INTO customerBooks(customerID, bookID)
+SELECT "000", bookID FROM book WHERE bookID = "000";
+
+INSERT INTO customerBooks(customerID, bookID)
+SELECT "000", bookID FROM book WHERE bookID = "001";
+
+INSERT INTO customerBooks(customerID, bookID)
+SELECT "001", bookID FROM book WHERE bookID = "002";
